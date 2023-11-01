8a5d7afcc345fa3c30c86d1ccaa8a19a05bd006f
JosiahBajnath <77852438+bhajicomp@users.noreply.github.com>
2023-11-01 12:04:24 -0400

    Added code to open the Words.txt file, read the words, and store them in a BST in Assignment1.cpp.

--- Assignment1-Files/Assignment1.cpp
+++ Assignment1-Files/Assignment1.cpp
@@ -6,19 +6,33 @@
 
 using namespace std;
 
+void readWordsAndStoreInBST(BTNode*& wordBST) {
+    ifstream wordsFile("/Assignment1-Files/Words.txt");
+    if (!wordsFile.is_open()) {
+        cerr << "Failed to open Words.txt" << endl;
+        return;
+    }
 
-int main() {
-	
+    string word;
+    while (wordsFile >> word) {
+        if (word == "END") {
+            break;
+        }
+        wordBST = insertBSTRec(wordBST, word);
+    }
 
-	BTNode * wordBST;
-	
-	cout << "COMP 2611 Assignment 1" << endl;
-	cout << "======================" << endl << endl;
-	
-	// read data from Words.txt and store in wordBST
-	
-	// read commands from Commands.txt and perform operations
-	
-	return 0;
+    wordsFile.close();
 }
 
+int main() {
+    BTNode * wordBST = NULL;
+    
+    cout << "COMP 2611 Assignment 1" << endl;
+    cout << "======================" << endl << endl;
+    
+    readWordsAndStoreInBST(wordBST);
+
+    // read commands from Commands.txt and perform operations
+    
+    return 0;
+}
